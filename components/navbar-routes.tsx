7a6f62a78404818e4fab378e426f032e0ab0a1e2
Maurice Consultant <mainna62@outlook.com>
2025-02-25 08:15:43 +0300

    feat. edit page

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -26,7 +26,7 @@ export const NavbarRoutes = () => {
         </div>
       ) : (
         <div className="flex gap-x-2 ml-auto">
-          {isEditorPage || isWriterPage ? (
+          {isEditorPage || isWriterPage && (
             <Link href={"/"}>
               <Button size={"sm"} variant={"ghost"}>
                 <LogOut className="h-4 w-4 mr-2" />
