a34cfb9e08257e03b44b95f1b97b85761d1cfdfa
Maurice Consultant <mainna62@outlook.com>
2025-02-25 08:17:49 +0300

    adj. navbar routes

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -6,11 +6,10 @@ import { Button } from "@/components/ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
 import { SearchInput } from "./search-input";
-import { isEditor } from "@/lib/editor";
 import { Suspense } from "react";
 
 export const NavbarRoutes = () => {
-  const { userId } = useAuth();
+  useAuth();
   const pathname = usePathname();
   const isEditorPage = pathname?.startsWith("/editor");
   const isWriterPage = pathname?.includes("/writers");
@@ -26,20 +25,20 @@ export const NavbarRoutes = () => {
         </div>
       ) : (
         <div className="flex gap-x-2 ml-auto">
-          {isEditorPage || isWriterPage && (
-            <Link href={"/"}>
-              <Button size={"sm"} variant={"ghost"}>
-                <LogOut className="h-4 w-4 mr-2" />
-                Exit
-              </Button>
-            </Link>
-          ) : isEditor(userId) ? (
+            {isEditorPage || isWriterPage && (
+              <Link href={"/"}>
+                <Button size={"sm"} variant={"ghost"}>
+                  <LogOut className="h-4 w-4 mr-2" />
+                  Exit
+                </Button>
+              </Link>
+            )} : isEditor(userId) ? (
             <Link href={"editor/writers"}>
               <Button size={"sm"} variant={"ghost"}>
                 Editor mode
               </Button>
             </Link>
-          ) : null}
+          ) : null
           <UserButton afterSwitchSessionUrl="/" />
         </div>)}
     </>
